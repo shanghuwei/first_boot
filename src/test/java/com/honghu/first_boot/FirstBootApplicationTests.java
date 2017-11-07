6e557424f3734791f5f13aea1fa0c7cfa86db11b
shanghuwei <350473059@qq.com>
2017-11-07 18:56:24 +0800

    单元测试

--- src/test/java/com/honghu/first_boot/FirstBootApplicationTests.java
+++ src/test/java/com/honghu/first_boot/FirstBootApplicationTests.java
@@ -1,16 +1,23 @@
 package com.honghu.first_boot;
 
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class FirstBootApplicationTests {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+public class FirstBootApplicationTests extends BaseTest{
 	@Test
-	public void contextLoads() {
+	public void test() throws Exception{
+		JSONObject girl = new JSONObject();
+		girl.put("age",14);
+		girl.put("cupSize" ,"F");
+		girl.put("honghu34" ,"54");
+		mockMvc.perform(post("/girl_add").content(girl.toJSONString()).header("Content-Type", "application/json;charset=UTF-8"))
+		.andDo(print())
+		.andExpect(status().isOk());
 	}
+
 
 }
